0aa79bd052880a30fe6c8d188033f672146779ae
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2023-02-05 16:03:35 +0900

    doc: tag, category 모델링

---
+++ src/models/category.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} Category
+ * @property {number} id
+ * @property {string} name
+ */
+
+/**
+ * @type {Category}
+ */
+const Category = {};
---
+++ src/models/tag.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} Tag
+ * @property {number} id
+ * @property {string} name
+ */
+
+/**
+ * @type {Tag}
+ */
+const Tag = {};
